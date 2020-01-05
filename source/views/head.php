5dd92cdf72459fc7a1b194c14bb0d9ecea5cc95d
kana2011th <khwd.2007@gmail.com>
2020-01-05 19:57:41 +0700

    forgot to change metadata
    Signed-off-by: kana2011th <khwd.2007@gmail.com>

--- source/views/head.php
+++ source/views/head.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
   <title><?php echo $config['title']; ?></title>
   <link rel="icon" href="<?php echo $config['logo']; ?>" type="image/icon type" />
-  <meta name="twitter:title" content="DriteShop : Home">
+  <meta name="twitter:title" content="<?php echo $config['title']; ?>">
   <meta property="og:image" content="assets/img/58625970_815194658863233_7215058884161961984_o.jpg">
   <meta name="description" content="ระบบเว็บซื้อขายของสินค้า By DriteStudio">
   <meta name="twitter:image" content="assets/img/58625970_815194658863233_7215058884161961984_o.jpg">
-  <meta name="twitter:description" content="Welcome to my world !">
+  <meta name="twitter:description" content="Welcome to webshop.">
   <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
