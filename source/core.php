359c96f0028fad9960317ad2bf2cd38327eadbc5
kana2011th <khwd.2007@gmail.com>
2020-02-08 16:22:31 +0700

    Add comments to core.php
    Signed-off-by: kana2011th <khwd.2007@gmail.com>

--- source/core.php
+++ source/core.php
@@ -3,11 +3,19 @@
 include 'coqnfig.php';
 session_start();
 if (!empty($_GET['page'])) {
+  // Main Component Pages
   $page = array('home', 'shop', 'topup', 'login');
   $ncheck = array('login');
+
+  // Add Admin's Page
   if ($_SESSION['isadmin'] === 1) array_push($page, 'admin-dashboard', 'admin-slide', 'admin-player', 'admin-server', 'admin-shop', 'admin-product', 'admin-editproduct', 'admin-createproductui', 'admin-serverinfo', 'admin-topup', 'admin-redeem', 'admin-checkin', 'admin-createcheckin', 'admin-editcheckin', 'admin-update', 'admin-news');
+
+  // Add User's Page if logged in
   if (!empty($_SESSION['id'])) array_push($page, 'history', 'redeem', 'checkin', 'truewallet', 'gift');
+
+  // it's time to include
   if (in_array($_GET['page'], $page)) include './include/' . $_GET['page'] . '.php';
+  // if page not found. fallback to main page...
   else include './include/home.php';
   if (!in_array($_GET['page'], $ncheck)) echo '<script>checklogin(\'' . $_SESSION['username'] . '\');</script>';
   die;
