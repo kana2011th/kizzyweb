78f8c3e21f93d31cceb18db8b4e8400a964d9ad0
kana2011th <khwd.2007@gmail.com>
2020-02-08 16:14:58 +0700

    Minor Refactor core.php

--- source/core.php
+++ source/core.php
@@ -2,29 +2,23 @@
 // ห้ามแก้ไข
 include 'coqnfig.php';
 session_start();
-if ($_GET['page'] != '') {
+if (!empty($_GET['page'])) {
   $page = array('home', 'shop', 'topup', 'login');
   $ncheck = array('login');
-  if ($_SESSION['isadmin'] == 1)
-    array_push($page, 'admin-dashboard', 'admin-slide', 'admin-player', 'admin-server', 'admin-shop', 'admin-product', 'admin-editproduct', 'admin-createproductui', 'admin-serverinfo', 'admin-topup', 'admin-redeem', 'admin-checkin', 'admin-createcheckin', 'admin-editcheckin', 'admin-update', 'admin-news');
-  if ($_SESSION['id'] != '')
-    array_push($page, 'history', 'redeem', 'checkin', 'truewallet', 'gift');
-  if (in_array($_GET['page'], $page))
-    include './include/' . $_GET['page'] . '.php';
-  else
-    include './include/home.php';
-  if (!in_array($_GET['page'], $ncheck))
-    echo '<script>checklogin(\'' . $_SESSION['username'] . '\');</script>';
+  if ($_SESSION['isadmin'] === 1) array_push($page, 'admin-dashboard', 'admin-slide', 'admin-player', 'admin-server', 'admin-shop', 'admin-product', 'admin-editproduct', 'admin-createproductui', 'admin-serverinfo', 'admin-topup', 'admin-redeem', 'admin-checkin', 'admin-createcheckin', 'admin-editcheckin', 'admin-update', 'admin-news');
+  if (!empty($_SESSION['id'])) array_push($page, 'history', 'redeem', 'checkin', 'truewallet', 'gift');
+  if (in_array($_GET['page'], $page)) include './include/' . $_GET['page'] . '.php';
+  else include './include/home.php';
+  if (!in_array($_GET['page'], $ncheck)) echo '<script>checklogin(\'' . $_SESSION['username'] . '\');</script>';
   die;
 }
-if ($_GET['shop'] != '' && is_numeric($_GET['shop'])) {
+if (!empty($_GET['shop']) && is_numeric($_GET['shop'])) {
   include './include/product.php';
   die('<script>swal.close();</script>');
 }
 function config($pdo, $name)
 {
-  if ($name  == '')
-    return 'Parameter error';
+  if (empty($name)) return 'Parameter error';
   $query = $pdo->prepare("SELECT `value` FROM `config` WHERE `name` = ?");
   $query->execute(array($name));
   return $query->fetch(PDO::FETCH_ASSOC)['value'];
